38833d8b299fb9ee055dcc79ff31e76cbf53cec5
EajayD <edelos1524@hotmail.com>
2021-08-29 22:37:34 -0700

    added query and mutations to typedefs

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -22,7 +22,20 @@ const typeDefs = gql`
     type Auth {
         token: ID!
         user: User
-    }`;
+    }
+
+    type Query {
+        me: User
+    }
+    
+    Mutation {
+        addUser(username: String!, email: String!, password: String!):Auth
+        login(username: String!, email: String!, password: String!):Auth
+        saveBook(bookId: String!, authors: [String]!, description: String!, title: String!, image: String!, link: String!): User
+        removeBook(bookId: String!): User
+    }
+    
+    `;
 
 module.exports = typeDefs;
     
